d4a9cdf0ed9b23e26ce977b608e4ecc79ab4671d
david <davidwdiniz@gmail.com>
2020-09-10 18:59:20 -0300

    importação de imagens e fontes externas

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,5 +1,16 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View`
+const Roboto = "RobotoSlab-Medium"
 
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #F4EDE8;
+  font-family: ${Roboto};
+  margin: 64px 0 24px;
 `;
