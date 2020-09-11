b5419d37cb174fa5b5afccc591a7aa70e35e59b4
david <davidwdiniz@gmail.com>
2020-09-11 11:27:31 -0300

    Criação da tela de cadastro

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Image, KeyboardAvoidingView, Platform, ScrollView, View} from "react-native";
 import Icon from "react-native-vector-icons/Feather";
+import {useNavigation} from "@react-navigation/native";
 
 import logoImg from "../../assets/logo.png";
 import Input from "../../components/Input";
@@ -8,6 +9,7 @@ import Button from "../../components/Button";
 import {Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText} from "./styles";
 
 const SignIn: React.FC = () => {
+    const navigation = useNavigation();
     return (
         <>
             <KeyboardAvoidingView
@@ -36,7 +38,7 @@ const SignIn: React.FC = () => {
                </ScrollView>
             </KeyboardAvoidingView>
 
-            <CreateAccountButton activeOpacity={1} onPress={() => {}}>
+            <CreateAccountButton activeOpacity={1} onPress={() => navigation.navigate("SignUp")}>
                     <Icon name="log-in" size={20} color="#FF9000" />
                     <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
             </CreateAccountButton>
