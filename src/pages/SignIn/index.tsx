43738dcbb7f57d0dd85bdbec1fd51a47b4b11ad6
david <davidwdiniz@gmail.com>
2020-09-13 11:00:37 -0300

    Autenticação de usuários

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -6,6 +6,7 @@ import {Form} from "@unform/mobile";
 import {FormHandles} from "@unform/core";
 import * as Yup from "yup";
 
+import {useAuth} from "../../hooks/auth";
 import logoImg from "../../assets/logo.png";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
@@ -21,6 +22,7 @@ const SignIn: React.FC = () => {
     const navigation = useNavigation();
     const formRef = useRef<FormHandles>(null);
     const passwordInputRef = useRef<TextInput>(null);
+    const {signIn} = useAuth();
 
     const handleSignIn = useCallback(async (data: SignInFormData) => {
         try {
@@ -30,11 +32,10 @@ const SignIn: React.FC = () => {
                 password: Yup.string().required("Senha obrigatória"),
             });
             await schema.validate(data, {abortEarly: false});
-            // await signIn({
-            //     email: data.email,
-            //     password: data.password,
-            // });
-            // history.push("/dashboard");
+            await signIn({
+                email: data.email,
+                password: data.password,
+            });
         } catch (err) {
             if (err instanceof Yup.ValidationError) {
                 const errors = getValidationErrors(err);
@@ -43,7 +44,7 @@ const SignIn: React.FC = () => {
             }
             Alert.alert("Erro na autenticação", "Ocorreu um erro ao fazer login, cheque as credenciais.");
         }
-    },[]);
+    },[signIn]);
 
     return (
         <>
