a62eaca07aea9f67e3b084959346b063e249b06f
david <davidwdiniz@gmail.com>
2020-12-01 12:55:31 -0300

    Estrutura da tela de confirmação

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,8 +1,50 @@
-import React from "react";
-import {View} from "react-native";
+import React, {useCallback, useMemo} from "react";
+import Icon from "react-native-vector-icons/Feather";
+import {Container, Title, Description, OkButton, OkButtonText} from "./styles";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import {format} from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
+interface RouteParams {
+    date: number;
+}
 
 const AppointmentCreated: React.FC = () => {
-    return <View />
+    const {reset} = useNavigation();
+    const {params} = useRoute();
+
+    const routParams = params as RouteParams;
+
+    const handleOkPressed = useCallback(() => {
+        reset({
+            routes: [
+                {
+                    name: "Dashboard"
+                }
+            ],
+            index: 0,
+        })
+    }, [reset]);
+
+    const formattedDate = useMemo(() => {
+        return format(
+            routParams.date,
+            "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h'",
+            {locale: ptBR}
+            );
+    }, [routParams.date]);
+
+    return (
+        <Container>
+            <Icon name="check" size={80} color="#04D361" />
+            <Title>Agendamento concluído</Title>
+            <Description>{formattedDate}</Description>
+
+            <OkButton onPress={handleOkPressed} >
+                <OkButtonText>Ok</OkButtonText>
+            </OkButton>
+        </Container>
+    );
 }
 
 export default AppointmentCreated;
